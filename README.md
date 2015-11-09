6ba1e68522c13b4db771b52f7678f0b45f1f0791
Matthew Hailwood <matthew@hailwood.nz>
2015-11-09 16:32:43 +1300

    v1.0.0 Release

---
+++ README.md
@@ -0,0 +1,47 @@
+#Silverstripe Carousel#
+This plugin simply adds the required dataobject and makes an extension available to automatically add a carousel tab to any page on your site.
+This plugin does not do the frontend for you at all as there are so many different options that it's impracticable.
+
+Just loop over `$PrimaryGalleryImages` or `$SecondaryGalleryImages` and render the slides out as you wish.
+`$Image` refers to the uploaded image
+`$Title` refers to the title on the image
+
+# Installation Instructions #
+## Composer ##
+Run the following to add this module as a requirement and install it via composer.
+
+```
+#!bash
+
+composer require "webfox/silverstripe-gallery"
+```
+
+Next up add the required config settings, below is an example of adding a carousel to all Page and all subclasses, setting a custom folder for
+the images to upload to, and setting custom tab titles:
+
+```
+Page:
+  primary_gallery:
+    title: 'Carousel Images'
+    folder: 'Carousel-Images'
+  secondary_gallery:
+    title: 'Gallery Images'
+    folder: 'Gallery-Images'
+  extensions:
+    - GalleryExtension
+```
+
+By defaults the galleries are disabled, but if you have applied the extension to a superclass You can disable the gallery on a subclass:
+
+```
+SubclassOfPage:
+  primary_gallery:
+    enabled: false
+  secondary_gallery:
+    enabled: false
+```
+
+then browse to /dev/build?flush=all
+
+#Requirements#
+* Silverstripe 3.1+
---
+++ code/PrimaryGalleryImage.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @method SiteTree Page()
+ */
+class PrimaryGalleryImage extends DataObject {
+
+    protected static $db = [
+        'Title'     => 'Varchar(255)',
+        'Displayed' => 'Boolean',
+        'SortOrder' => 'Int'
+    ];
+
+    protected static $has_one = [
+        'Page'  => 'Page',
+        'Image' => 'Image'
+    ];
+
+    private static $defaults = [
+        'Displayed' => true
+    ];
+
+    protected static $default_sort = 'SortOrder';
+
+    public function getCMSFields() {
+
+        $config = $this->Page()->config()->get("primary_gallery") ?: [];
+        if(!is_array($config) || !isset($config['folder'])) $config = ['folder' => 'Primary-Gallery-Images'];
+
+        $fields = new FieldList([
+            TextField::create('Title', 'Title/Caption'),
+            DropdownField::create('Displayed', 'Displayed', [1 => 'Displayed', 0 => 'Hidden']),
+            UploadField::create('Image', 'Image')->setFolderName("Images/{$config['folder']}")
+        ]);
+        
+        $this->extend('updateCMSFields', $fields);
+
+        return $fields;
+
+    }
+
+    public function canView($member = null) {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canEdit($member = null) {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canDelete($member = null) {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+    public function canCreate($member = null) {
+        return Permission::check('CMS_ACCESS_CMSMain', 'any', $member);
+    }
+
+}
