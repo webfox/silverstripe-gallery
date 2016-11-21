33e08bcda433945975f02bd08b14d2d0ae6d1dff
Otis Wright <otis@webfox.co.nz>
2016-11-21 13:56:26 +1300

    Update GalleryExtension.php

--- code/GalleryExtension.php
+++ code/GalleryExtension.php
@@ -7,28 +7,28 @@ class GalleryExtension extends SiteTreeExtension
 {
 
     private static $has_many = [
-        'PrimaryGalleryImages'   => 'PrimaryGalleryImage',
+        'PrimaryGalleryImages' => 'PrimaryGalleryImage',
         'SecondaryGalleryImages' => 'SecondaryGalleryImage',
     ];
 
     public function updateCMSFields(FieldList $fields)
     {
         foreach (['primary', 'secondary'] as $lower) {
-            $upper  = ucfirst($lower);
-            
+            $upper = ucfirst($lower);
+
             $fields->removeByName("{$upper}GalleryImages");
             $config = $this->owner->config()->get("{$lower}_gallery");
             if (is_null($config) || (isset($config['enabled']) && $config['enabled'] === false)) {
                 continue;
             }
 
-            $config['title']  = isset($config['title']) ? $config['title'] : "{$upper} Gallery";
+            $config['title'] = isset($config['title']) ? $config['title'] : "{$upper} Gallery";
             $config['folder'] = isset($config['folder']) ? $config['folder'] : "{$upper}-Gallery-Images";
 
             $GridFieldConfig = new GridFieldConfig_RecordEditor();
             $GridFieldConfig->removeComponentsByType('GridFieldAddNewButton');
             $GridFieldConfig->addComponent($bulkUploadConfig = new GridFieldBulkUpload());
-            $GridFieldConfig->addComponent(new GridFieldSortableRows('SortOrder'));
+            $GridFieldConfig->addComponent(new GridFieldOrderableRows('SortOrder'));
             $GridFieldConfig->addComponent(new GridFieldGalleryTheme('Image'));
             $bulkUploadConfig->setUfSetup('setFolderName', "Images/{$config['folder']}");
             $GridField = new GridField("{$upper}GalleryGridField", $config['title'], $this->owner->{"{$upper}GalleryImages"}(), $GridFieldConfig);
@@ -36,15 +36,14 @@ class GalleryExtension extends SiteTreeExtension
             /** @var TabSet $rootTab */
             //We need to repush Metadata to ensure it is the last tab
             $rootTab = $fields->fieldByName('Root');
-            $rootTab->push($tab = Tab::create("{$upper}Gallery"));
-            if ($rootTab->fieldByName('Metadata')) {
-                $metaChildren = $rootTab->fieldByName('Metadata')->getChildren();
-                $rootTab->removeByName('Metadata');
-                $rootTab->push(Tab::create('Metadata')->setChildren($metaChildren));
+            if ($this->owner->exists()) {
+                $rootTab->push($tab = Tab::create("{$upper}Gallery"));
             }
 
-            $tab->setTitle($config['title']);
-            $fields->addFieldToTab("Root.{$upper}Gallery", $GridField);
+            if ($this->owner->exists()) {
+                $fields->addFieldToTab("Root.{$upper}Gallery", $GridField);
+                $tab->setTitle($config['title']);
+            }
         }
     }
 }
